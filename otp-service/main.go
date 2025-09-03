240d08964b2865b1864072b9b06918512758c959
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-09-03 21:20:59 +0330

    added pagination route

--- otp-service/main.go
+++ otp-service/main.go
@@ -3,17 +3,22 @@ package main
 
 import (
     "otp-service/config"
+    "otp-service/models"
     "otp-service/handlers"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
     config.InitRedis()
+    models.InitUsers()
 
     router := gin.Default()
+    
     router.POST("/request-otp", handlers.RequestOTP) // Request OTP
     router.POST("/validate-otp", handlers.ValidateOTP) // Validate OTP and login/register
     router.GET("/user", handlers.GetSingleUserByPhone)
+    router.GET("/users", handlers.GetUsersWithPagination) // Get paginated list of users
+
 
     router.Run(":8080") // Start server on port 8080
 }
